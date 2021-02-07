2a0b1bc13fa3c1af3a644e6b225c6491cbfcaa9c
Dongruixuan Li <dongruixuan@hotmail.com>
2021-02-08 01:52:30 +0800

    fix patch_all.sh
    Signed-off-by: Dongruixuan Li <dongruixuan@hotmail.com>

--- deploy/patch_all.sh
+++ deploy/patch_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kubectl patch -n catmeow-auth -f mongo/mongo.yaml
-kubectl patch -n catmeow-auth -f openid/openid.yaml
-kubectl patch -n catmeow-auth -f usercenter_backend/backend.yaml
-kubectl patch -n catmeow-auth -f usercenter_frontend/frontend.yaml
+kubectl apply -n catmeow-auth -f mongo/mongo.yaml
+kubectl apply -n catmeow-auth -f openid/openid.yaml
+kubectl apply -n catmeow-auth -f usercenter_backend/backend.yaml
+kubectl apply -n catmeow-auth -f usercenter_frontend/frontend.yaml
